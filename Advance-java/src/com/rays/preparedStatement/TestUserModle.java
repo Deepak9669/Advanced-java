0a9543a8c526ecfb4f22c6c71b69c9aec251e60e
Deepak9669 <deepakverma726800@gmail.com>
2025-09-18 20:02:35 +0530

    done

--- Advance-java/src/com/rays/preparedStatement/TestUserModle.java
+++ Advance-java/src/com/rays/preparedStatement/TestUserModle.java
@@ -1,11 +1,29 @@
 package com.rays.jdbc.preparedStatement;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class TestUserModle {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws Exception {
+		testAdd();
+
+	}
+	public static void testAdd() throws ParseException, ClassNotFoundException, SQLException {
+		
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+		
 		UserModel model=new UserModel();
-		System.out.println(model.nextpk());
+		UserBean bean=new UserBean();
+		
+		bean.setId(4);
+		bean.setFirstName("Deepak");
+		bean.setLastName("verma");
+		bean.setLogin("deepak@gmail.com123");
+		bean.setPassword("dev123");		
+		bean.setDob(sdf.parse("2002-06-01"));
+		
+		model.add(bean);
 	}
 
 }
--- Advance-java/src/com/rays/preparedStatement/UserModel.java
+++ Advance-java/src/com/rays/preparedStatement/UserModel.java
@@ -1,6 +1,7 @@
 package com.rays.jdbc.preparedStatement;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
 
 
 public class UserModel {
@@ -22,6 +23,29 @@ public class UserModel {
 		}
 		conn.close();
 		return pk +1;
+	}
+	public void add(UserBean bean) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","root");
+		
+	PreparedStatement psmt=	conn.prepareStatement("insert into st_user values(?,?,?,?,?,?)");
+	
+	     int pk=nextpk(); 
+	     
+	     psmt.setInt(1, pk);
+	     psmt.setString(2, bean.getFirstName());
+	     psmt.setString(3, bean.getLastName());
+	     psmt.setString(4, bean.getLogin());
+	     psmt.setString(5, bean.getPassword());
+	     psmt.setDate(6, new java.sql.Date(bean.getDob().getTime()));
+	
+	int i =	psmt.executeUpdate();
+	System.out.println("insert data sucessfully :"+i);
+	
+	conn.close();
+		
 	}
 
 }
