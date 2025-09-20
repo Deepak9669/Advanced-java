484e86f1c61f760642b9b17c2eb2080e096a9554
Deepak9669 <deepakverma726800@gmail.com>
2025-09-20 10:45:37 +0530

    done

--- Advance-java/src/com/rays/preparedStatement/TestUserModle.java
+++ Advance-java/src/com/rays/preparedStatement/TestUserModle.java
@@ -6,24 +6,101 @@ import java.text.SimpleDateFormat;
 
 public class TestUserModle {
 	public static void main(String[] args) throws Exception {
-		testAdd();
-
+//		 testFindByLogin();
+//		 testAdd();
+//	     testUpdate();
+//		 testDelete();
+		testAutenticate();
+		
 	}
-	public static void testAdd() throws ParseException, ClassNotFoundException, SQLException {
+	public static void testAdd() throws Exception {
 		
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
 		
 		UserModel model=new UserModel();
 		UserBean bean=new UserBean();
 		
-		bean.setId(4);
-		bean.setFirstName("Deepak");
+		System.out.println(model.nextpk());
+		
+		bean.setId(5);
+		bean.setFirstName("sumit");
 		bean.setLastName("verma");
-		bean.setLogin("deepak@gmail.com123");
+		bean.setLogin("sumit@gmail.com123");
 		bean.setPassword("dev123");		
-		bean.setDob(sdf.parse("2002-06-01"));
+		bean.setDob(sdf.parse("2000-06-01"));
 		
 		model.add(bean);
 	}
+//	test delete record
+	
+	public static void testDelete() throws ClassNotFoundException, SQLException {
+		UserModel model=new UserModel();
+		UserBean bean=new UserBean();
+		
+		System.out.println(model.nextpk());
+		
+		bean.setId(12);
+		
+		model.delete(bean);
+		
+		
+	}
+//	test update records
+	
+	public static void testUpdate() throws ClassNotFoundException, SQLException, ParseException {
+		
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+		
+		UserModel model = new UserModel();
+		UserBean bean = new UserBean();
+		
+		System.out.println(model.nextpk());
+		
+		bean.setId(3);
+		bean.setFirstName("devraj");
+		bean.setLastName("pandit ji");
+		bean.setLogin("devil@1234");
+		bean.setPassword("1235");
+		bean.setDob(sdf.parse("2004-06-18"));
+		
+		model.update(bean);
+	}
+	public static void testFindByLogin() throws Exception {
+		
+		UserModel model = new UserModel();
+		System.out.println(model.nextpk());
 
-}
+		
+		UserBean existsBean = model.findByLogin("ram@gmail.com");
+		
+		if(existsBean != null) {
+			System.out.println("login id is already exist");
+		} else {
+			System.out.println("no record found");
+
+		
+		
+		}
+	}
+	public static void testAutenticate() throws Exception {
+		
+		UserModel model = new UserModel();
+//		System.out.println(model.nextpk());
+
+		
+		UserBean existsBean = model.autenticate("ram@gmail.com" ,"ram123 ");
+		
+		if(existsBean != null) {
+			System.out.println("login sucesfull");
+		} else {
+			System.out.println("Wrong password and user name");
+
+		
+		
+		}
+	}
+
+
+	}
+
+
--- Advance-java/src/com/rays/preparedStatement/UserModel.java
+++ Advance-java/src/com/rays/preparedStatement/UserModel.java
@@ -24,7 +24,16 @@ public class UserModel {
 		conn.close();
 		return pk +1;
 	}
-	public void add(UserBean bean) throws ClassNotFoundException, SQLException {
+	public void add(UserBean bean) throws Exception {
+		
+		
+	UserBean existsBean = findByLogin(bean.getLogin());	
+//		UserBean existsBean=authenticate(bean.getPassword());      
+
+            if (existsBean != null) {
+				throw new Exception("login id already exist");
+
+		}
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
@@ -45,7 +54,100 @@ public class UserModel {
 	System.out.println("insert data sucessfully :"+i);
 	
 	conn.close();
+		}	
+	
+	public void delete(UserBean bean) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","root");
+		
+	PreparedStatement psmt=	conn.prepareStatement("delete from st_user where id=?");
+	
+	psmt.setInt(1, bean.getId());
+	
+	int i=psmt.executeUpdate();
+	System.out.println("data delete sucessfully "+i);
+	
+	conn.close();
 		
 	}
+	public void update(UserBean bean) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","root");
+		
+	PreparedStatement psmt=	conn.prepareStatement("update st_user set firstName=?,lastName=?,login=?,password=?,dob=? where id=?");
+          
+	   psmt.setString(1, bean.getFirstName());
+	   psmt.setString(2, bean.getLastName());
+	   psmt.setString(3, bean.getLogin());
+	   psmt.setString(4, bean.getPassword());
+	   psmt.setDate(5, new java.sql.Date(bean.getDob().getTime()));
+	   psmt.setInt(6, bean.getId());
+	   
+	   int i=psmt.executeUpdate();
+	   System.out.println("data update sucesfully :"+i);
+	   
+	   conn.close();
+		
+	}
+	public UserBean findByLogin(String login) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","root");
+		
+	PreparedStatement psmt=	conn.prepareStatement("select * from st_user where login=?");
+	
+	  psmt.setString(1, login);
+	  
+	  ResultSet rs=psmt.executeQuery();
+	  
+	  UserBean bean=null;
+	  
+	  while(rs.next()) {
+		  bean=new UserBean();
+		  
+      bean.setId(rs.getInt(1));
+      bean.setFirstName(rs.getString(2));
+      bean.setLastName(rs.getString(3));
+      bean.setLogin(rs.getString(4));
+      bean.setPassword(rs.getString(5));
+      bean.setDob(rs.getDate(6));
+		   
 
+	}
+	  return bean;
+
+}
+	    public UserBean autenticate(String login,String password) throws ClassNotFoundException, SQLException {
+	    	
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","root");
+			
+		PreparedStatement psmt=	conn.prepareStatement("select * from st_user where login=? and password=?");
+		
+		psmt.setString(1, login);
+		psmt.setString(2, password);
+		
+		ResultSet rs=psmt.executeQuery();
+          
+		UserBean bean=null;
+		
+		while(rs.next()) {
+			bean = new UserBean();
+			
+		      bean.setId(rs.getInt(1));
+		      bean.setFirstName(rs.getNString(2));
+		      bean.setLastName(rs.getNString(3));
+		      bean.setLogin(rs.getNString(4));
+		      bean.setPassword(rs.getString(5));
+		      bean.setDob(rs.getDate(6));
+
+			
+		}
+	    	return bean;
+	    }
+	
 }
